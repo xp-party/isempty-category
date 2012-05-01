cb9708befa582e5d86b9905c10673b63d2037f24
Anton Katkov <katkov.anton@gmail.com>
2012-05-01 12:40:17 +0400

    Initial Commit

---
+++ isempty-categoryTests/isempty_categoryTests.h
@@ -0,0 +1,13 @@
+//
+//  isempty_categoryTests.h
+//  isempty-categoryTests
+//
+//  Created by Vanger on 01.05.12.
+//  Copyright (c) 2012 Flexis. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface isempty_categoryTests : SenTestCase
+
+@end
---
+++ isempty-categoryTests/isempty_categoryTests.m
@@ -0,0 +1,32 @@
+//
+//  isempty_categoryTests.m
+//  isempty-categoryTests
+//
+//  Created by Vanger on 01.05.12.
+//  Copyright (c) 2012 Flexis. All rights reserved.
+//
+
+#import "isempty_categoryTests.h"
+
+@implementation isempty_categoryTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in isempty-categoryTests");
+}
+
+@end
